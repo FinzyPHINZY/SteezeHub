13123e3aeeb49d9b8f78c376da77f6efefcfee0f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-04 12:39:47 +0100

    added starter files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>let's fucking go'</h1>
+      <button>click me</button>
+    </>
+  );
+}
+
+export default App;
