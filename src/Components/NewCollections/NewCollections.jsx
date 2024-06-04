9ec798035a027c1929776bb7fbf2f37bc4428e17
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-04 16:09:49 +0100

    Implemented homepage

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -11,6 +11,7 @@ const NewCollections = () => {
         name={item.name}
         image={item.image}
         new_price={item.new_price}
+        old_price={item.old_price}
       />
     );
   });
