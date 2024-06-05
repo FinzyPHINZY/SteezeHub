a6e4ac09dd3aae6c41b984e52510b30fa97fe453
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-05 04:32:46 +0100

    built signup-login wireframe

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,7 +1,26 @@
 import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
-  return <div>loginsignup</div>;
+  return (
+    <div className="loginSignup">
+      <div className="loginSignup__container">
+        <h1>Sign Up</h1>
+        <div className="loginSignup__fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginSignup__login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginSignup__agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
