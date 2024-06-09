da951dc52ce2a95d303bc3aff44fd884d1cf8bcc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-09 11:24:01 +0100

    Functionality: Rendered Popular Items from server

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,17 @@
 import "./Popular.css";
-import data from "../Assets/data";
 import Item from "../Items/Item";
+import { useEffect, useState } from "react";
 
 const Popular = () => {
-  const products = data.map((item) => {
+  const [popularItems, setPopularItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/product/popularwomen")
+      .then((res) => res.json())
+      .then((data) => setPopularItems(data.data));
+  }, []);
+
+  const products = popularItems.map((item) => {
     return <Item key={item.id} item={item} />;
   });
 
