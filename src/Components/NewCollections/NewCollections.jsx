0760e9728a27dea8452af290f62d600eff265995
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-09 04:19:42 +0100

    Implemented  fetching NewCollection items from server

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,11 +1,20 @@
 import "./NewCollections.css";
-import new_collection from "../Assets/new_collections";
 import Item from "../Items/Item";
+import { useEffect, useState } from "react";
 
 const NewCollections = () => {
+  const [new_collection, setNew_collection] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/product/newcollections")
+      .then((res) => res.json())
+      .then((data) => setNew_collection(data.data));
+  }, []);
+
   const products = new_collection.map((item) => {
     return <Item key={item.id} item={item} />;
   });
+
   return (
     <div className="new__collections">
       <h1>new collections</h1>
