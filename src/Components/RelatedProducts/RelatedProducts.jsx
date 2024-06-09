52f5add8679ba530fd70c369fd182b3cb2d4bd67
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-09 21:32:44 +0100

    Updated related products functionality

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,8 +1,23 @@
 import "./RelatedProducts.css";
-import data from "../Assets/data";
+// import data from "../Assets/data";
 import Item from "../Items/Item";
+import { useEffect, useState } from "react";
 
 const RelatedProducts = () => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/product/")
+      .then((res) => res.json())
+      .then((output) => {
+        setAllProducts(output.data);
+      });
+  }, []);
+
+  const random = Math.floor(Math.random() * 32);
+
+  const data = allProducts.slice(random, random + 4);
+
   const products = data.map((item) => <Item key={item.id} item={item} />);
   return (
     <div className="related">
